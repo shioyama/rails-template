95f9c150d31479c2ecb1a26e71bfe63fa4e95e88
Chris Salzberg <chris@dejimata.com>
2019-01-15 14:43:19 +0900

    Fill out webpack setup

--- template.rb
+++ template.rb
@@ -23,13 +23,15 @@ gem_names << "actionmailer"  unless options[:skip_action_mailer]
 gem_names << "actioncable"   unless options[:skip_action_cable]
 gem_names << "activestorage" unless options[:skip_active_storage]
 
-gemfile_gems = gem_names.inject("") { |gems, gem_name| gems << "gem \"#{gem_name}\", \"#{RAILS_VERSION}\"\n" }
+gemfile_gems = gem_names.inject("") do |gems, gem_name|
+  gems << "gem \"#{gem_name}\", \"#{RAILS_VERSION}\"\n"
+end
 
 in_root do
-  gsub_file "Gemfile", /^gem 'rails'.*$/, gemfile_gems, verbose: false
-  gsub_file "Gemfile", /^gem 'jbuilder'/, "# gem 'jbuilder'", verbose: false if options[:skip_javascript]
+  gsub_file "Gemfile", /^gem 'rails'.*$/, gemfile_gems
+  gsub_file "Gemfile", /^gem 'jbuilder'/, "# gem 'jbuilder'" if options[:skip_javascript]
   # Replace byebug with pry-byebug, which anyway depends on byebug
-  gsub_file "Gemfile", /gem 'byebug'/, "gem 'pry-byebug'", verbose: false
+  gsub_file "Gemfile", /gem 'byebug'/, "gem 'pry-byebug'"
   gsub_file "Gemfile", /^group :development, :test do$/, <<-EOF
 group :development, :test do
   gem 'rspec-rails'
@@ -61,9 +63,17 @@ end
 
 after_bundle do
   if options[:webpack]
+    run "rails webpacker:install:erb"
+    #remove_file "frontend/packs/hello_erb.js.erb"
+
     # See: https://evilmartians.com/chronicles/evil-front-part-1
-    gsub_file "config/webpacker.yml", /source_path\: app\/javascript/, "source_path: frontend", verbose: false
+    gsub_file "config/webpacker.yml", /source_path\: app\/javascript/, "source_path: frontend"
+
     run "mv app/javascript frontend"
+    run "mkdir frontend/site"
+    run "mkdir frontend/components"
+    run "mkdir frontend/init"
+    run "mkdir frontend/images"
 
     create_file 'semantic.json' do <<-EOF
 {
@@ -92,10 +102,74 @@ after_bundle do
 EOF
     end
 
-    run "yarn add jquery less less-loader rails-erb-loader rails-ujs resolve-url-loader #{"turbolinks " unless options[:skip_turbolinks]}--save"
+    npm_packages = %w[jquery less less-loader rails-erb-loader rails-ujs resolve-url-loader]
+    npm_packages += ["turbolinks"] unless options[:skip_turbolinks]
+    npm_packages += ["activestorage"] unless options[:skip_active_storage]
+    run "yarn add #{npm_packages.join(' ')} --save"
     run "yarn add semantic-ui --dev"
 
     run "cp node_modules/semantic-ui/src/theme.config.example frontend/theme.config"
+    gsub_file "frontend/theme.config", /^@siteFolder\s+:\s+'site';$/, "@siteFolder  :  '../../site';"
+    gsub_file "frontend/theme.config", /^@import\s.*\s\"theme.less\";$/, "@import (multiple) \"./semantic/src/theme.less\";"
+    append_file "frontend/theme.config", "\n@fontPath : \"../../../themes/@{theme}/assets/fonts\";"
+
+    insert_into_file "config/webpack/environment.js", <<-EOF, before: "module.exports"
+
+const less = require('./loaders/less')
+environment.loaders.append('less', less)
+
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  jquery: 'jquery'
+}))
+
+environment.config.set('resolve.alias', {
+  '../../theme.config': '../../../../theme.config'
+})
+
+EOF
+
+    create_file "config/webpack/loaders/less.js" do <<-EOF
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  test: /\.less$/,
+  use: ExtractTextPlugin.extract({
+    use: ['css-loader', 'less-loader']
+  })
+}
+EOF
+    end
+
+    create_file "frontend/init/index.js" do <<-EOF
+import "./index.scss"
+EOF
+    end
+    create_file "frontend/init/index.scss"
+
+    remove_file "frontend/packs/application.js"
+    create_file "frontend/packs/application.js.erb" do <<-EOF
+import 'semantic/src/semantic.less'
+// Import whatever semantic ui modules you need, e.g.:
+// import 'semantic/src/definitions/behaviors/api'
+
+import "init"
+
+// Import all images in frontend/images
+<% images = Webpacker.config.source_path.join('images').glob('**/*.{png,svg}') %>
+<% images.each do |image| %>
+  import '<%= image %>';
+<% end %>
+
+import Rails from 'rails-ujs'
+Rails.start()
+#{%q{
+import Turbolinks from 'turbolinks'
+Turbolinks.start()} unless options[:skip_turbolinks]}
+EOF
+    end
   end
 
   # Remove sprockets assets path if we're skipping sprockets
@@ -115,8 +189,7 @@ EOF
 
       # Set generator
       config.generators do |g|#{%q{
-        g.orm :active_record
-} unless options[:skip_active_record] }
+        g.orm :active_record} unless options[:skip_active_record] }
         g.template_engine :slim
         g.test_framework :rspec, :fixture => true
         g.fixture_replacement :factory_bot, :dir => "spec/factories"
@@ -132,8 +205,7 @@ EOF
         g.stylesheets      false
         g.javascripts      false
         g.helper           false
-        g.channel          assets: false
-} if options[:skip_sprockets] }
+        g.channel          assets: false} if options[:skip_sprockets] }
       end
 EOF
   end
@@ -150,7 +222,7 @@ EOF
   generate 'rspec:install'
   run "echo '--color -f d' > .rspec"
 
-  # # Git
+  # Git
   git init: "--quiet"
   git add: ".", commit: %(--all --no-verify --quiet --message "Initial commit.")
 end
